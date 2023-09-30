7b4448a35f0f53600180d1aa7875a60d68c0bd63
LeRyokan <tixier.lucas@gmail.com>
2023-09-30 10:30:11 +0200

    Add tilemap + bat

--- Assets/Scripts/StartZone.cs
+++ Assets/Scripts/StartZone.cs
@@ -1,9 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class StartZone : MonoBehaviour
 {
+
+    [SerializeField] private BatController playerBat;
+
+    private void Awake()
+    {
+        playerBat.transform.position = transform.position;
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
